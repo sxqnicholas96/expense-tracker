a98d9efa5534d53383ff2dbf0888885df24755cb
sxqnicholas96 <sxq.nicholas96@gmail.com>
2021-03-23 01:44:19 +0800

    add routes dashboard + transactions

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,26 @@
 import React from "react"
-import { BrowserRouter, Switch } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+enum Routes {
+  DASHBOARD = "/dashboard",
+  TRANSACTIONS = "/transactions",
+}
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
-      <Switch></Switch>
+      <Switch>
+        <Route
+          exact
+          path={Routes.DASHBOARD}
+          component={() => <div> Dashboard</div>}
+        />
+        <Route
+          exact
+          path={Routes.TRANSACTIONS}
+          component={() => <div> Transactions</div>}
+        />
+      </Switch>
     </BrowserRouter>
   )
 }
