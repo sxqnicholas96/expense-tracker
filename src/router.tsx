f0d20af935849253b0a6987cae0cb7e21898629f
sxqnicholas96 <sxq.nicholas96@gmail.com>
2021-03-23 01:40:04 +0800

    AppRouter code skeleton

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrowserRouter, Switch } from "react-router-dom"
+
+const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch></Switch>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
