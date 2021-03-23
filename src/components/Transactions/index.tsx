9c8792abc6d96fb58d9d7669bcdc0fd5ad94c5be
sxqnicholas96 <sxq.nicholas96@gmail.com>
2021-03-23 08:57:53 +0800

    rough improved design of Transactions

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -2,25 +2,42 @@ import React from "react"
 import { Table } from "antd"
 import { ColumnsType } from "antd/es/table"
 
+import store from "src/store"
 import { Transaction } from "src/types"
+
+import styles from "./style.module.css"
 
 const columns: ColumnsType<Transaction> = [
   {
     title: "Date",
     dataIndex: "date",
     key: "date",
-    render: (text: string) => <span>{text}</span>,
+    render: (date: Date) => <span>{date.toDateString()}</span>,
   },
   {
     title: "Nett Transaction Amount",
     dataIndex: "transactionAmount",
     key: "transactionAmount",
-    render: (amount: number) => <span>{amount}</span>,
+    render: (amount: number) => {
+      const className = amount >= 0 ? styles.depositStyle : styles.withdrawStyle
+
+      return <span className={className}>{amount}</span>
+    },
   },
 ]
 
 const Transactions: React.FC = () => {
-  return <Table columns={columns}></Table>
+  const transactionData = store.get("transactions")
+  const balance = store.get("balance")
+
+  return (
+    <div className={styles.container}>
+      <h1>Your Balance : ${balance} SGD</h1>
+      <div className={styles.tableContainer}>
+        <Table columns={columns} dataSource={transactionData}></Table>
+      </div>
+    </div>
+  )
 }
 
 export default Transactions
