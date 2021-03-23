a53feb2a4c4725daf645b6ab7b253468657ea934
sxqnicholas96 <sxq.nicholas96@gmail.com>
2021-03-23 16:00:24 +0800

    add transaction modal

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { Table } from "antd"
+import React, { useState } from "react"
+import { Button, Table, Modal } from "antd"
+import { PlusCircleOutlined } from "@ant-design/icons"
 import { ColumnsType } from "antd/es/table"
 
 import store from "src/store"
@@ -27,12 +28,32 @@ const columns: ColumnsType<Transaction> = [
 ]
 
 const Transactions: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
   const transactionData = store.get("transactions")
   const balance = store.get("balance")
 
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const closeModal = () => {
+    setIsModalVisible(false)
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Your Balance : ${balance} SGD</h1>
+      <div className={styles.headerContainer}>
+        <h1>Your Balance : ${balance} SGD</h1>
+        <Modal
+          title="Add Transaction"
+          visible={isModalVisible}
+          onOk={closeModal}
+          onCancel={closeModal}
+        ></Modal>
+        <Button onClick={showModal}>
+          Add Transaction <PlusCircleOutlined />
+        </Button>
+      </div>
       <div className={styles.tableContainer}>
         <Table columns={columns} dataSource={transactionData}></Table>
       </div>
